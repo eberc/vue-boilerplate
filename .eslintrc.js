1fc222310b441bdae1a389fae7ef157e21974000
Eber Camargo <bodacamargo@gmail.com>
2020-03-06 15:26:21 -0300

    lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,23 @@
 module.exports = {
+  settings: {
+    "import/resolver": {
+      alias: {
+        map: [
+          ["@","src"],
+          ["@assets", "src/assets/"],
+          ["@components", "src/components"],
+          ["@sections", "src/sections"],
+          ["@styles", "src/assets/styles"]
+        ],
+        extensions: [".vue", ".scss", ".js"]
+      }
+    }
+  },
   root: true,
   env: {
-    node: true,
+    browser: true,
+    es6: true,
+    node: true
   },
   extends: [
     'plugin:vue/essential',
@@ -13,5 +29,17 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'import/no-unresolved': 'off',
+    'max-len': 'off',
+    'no-unused-vars': 'off',
+    'linebreak-style': [
+      0,
+      'error',
+      'windows'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ]
   },
 };
