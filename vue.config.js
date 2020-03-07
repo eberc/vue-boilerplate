323b5b5499b56a84ebc1b6336c83eae5986a75a5
Eber Camargo <bodacamargo@gmail.com>
2020-03-06 22:36:45 -0300

    add express server

--- vue.config.js
+++ vue.config.js
@@ -3,15 +3,15 @@ const path = require('path')
 module.exports = {
   publicPath: '',
   configureWebpack: {
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@assets': path.resolve(__dirname, 'src/assets'),
-      '@styles': path.resolve(__dirname, 'src/assets/styles'),
-      '@components': path.resolve(__dirname, 'src/components'),
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '@assets': path.resolve(__dirname, 'src/assets'),
+        '@styles': path.resolve(__dirname, 'src/assets/styles'),
+        '@components': path.resolve(__dirname, 'src/components'),
+      },
     },
   },
-}
   /* chainWebpack: (config) => {
     config.resolve.alias
       .set('@', path.resolve(__dirname, 'src'))
