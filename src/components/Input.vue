c0cf83cb24f1fc6d230e4b4e30837125feaa625a
eber404 <bodacamargo@gmail.com>
2020-09-23 12:35:46 -0300

    two-way databinding example

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,10 +1,18 @@
-<template lang="pug">
-    <input class="default-input">
+<template>
+<div>
+  <h2 class="my-text"> {{ insideText }} </h2>
+  <input class="input-default" v-model="insideText">
+</div>
 </template>
 
 <script>
 export default {
   name: 'VueInput',
+  data() {
+    return {
+      insideText: 'Digite alguma coisa',
+    }
+  },
 }
 </script>
 
@@ -16,5 +24,13 @@ export default {
     height: 45px;
     padding: 10px;
     font-family: 'Roboto';
+    border: 1px solid;
+    border-radius: 5px;
+  }
+
+  .my-text {
+    font-size: 32px;
+    color: get($colors, text);
+    padding: 10px;
   }
 </style>
